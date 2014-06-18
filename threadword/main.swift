8a7892de1e61ad56a86dd5a84a12a84be072700a
Josh Oldenburg <joshthegeek@me.com>
2014-06-18 11:23:47 -0400

    Initial Commit

---
+++ threadword/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  threadword
+//
+//  Created by Joshua Oldenburg on 6/18/14.
+//  Copyright (c) 2014 Joshua Oldenburg. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
